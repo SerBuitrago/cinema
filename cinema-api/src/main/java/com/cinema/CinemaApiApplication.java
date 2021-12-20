c1d4e3bd79e08f2f93359b27c6076bab3c28b527
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-12-20 06:53:47 -0500

    Init

---
+++ cinema-api/src/main/java/com/cinema/CinemaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApiApplication.class, args);
+	}
+
+}
---
+++ cinema-api/src/test/java/com/cinema/CinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
