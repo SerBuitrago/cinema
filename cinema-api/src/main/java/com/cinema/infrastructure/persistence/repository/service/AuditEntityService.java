97574fde298b33b21384fc3d320a70dbd8fe62d1
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-12-22 17:16:00 -0500

    Api audit

--- cinema-api/src/main/java/com/cinema/infrastructure/persistence/repository/service/AuditEntityService.java
+++ cinema-api/src/main/java/com/cinema/infrastructure/persistence/repository/service/AuditEntityService.java
@@ -7,25 +7,31 @@ import org.springframework.stereotype.Service;
 
 import com.cinema.application.repository.AuditRepository;
 import com.cinema.dominio.entity.Audit;
+import com.cinema.infrastructure.exception.CinemaException;
 import com.cinema.infrastructure.persistence.entity.AuditEntity;
+import com.cinema.infrastructure.persistence.entity.validate.AuditEntityValidate;
 import com.cinema.infrastructure.persistence.mapper.AuditEntityMapper;
 import com.cinema.infrastructure.persistence.repository.AuditEntityRepository;
 import com.cinema.infrastructure.util.Cinema;
+import com.cinema.infrastructure.util.CinemaDate;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class AuditEntityService implements AuditRepository{
-	
+public class AuditEntityService implements AuditRepository {
+
 	private final AuditEntityRepository auditEntityRepository;
 	private final AuditEntityMapper auditEntityMapper;
-	
+
 	@Override
 	public Audit findById(Long id) {
+		if (!Cinema.isLong(id))
+			throw new CinemaException("El id de la auditoria no es valido.");
 		Optional<AuditEntity> optional = auditEntityRepository.findById(id);
-		AuditEntity audit = optional.orElse(null);
-		return auditEntityMapper.toDomain(audit);
+		if (!optional.isPresent())
+			throw new CinemaException("No se ha encontrado ninguna auditoria con el id " + id + ".");
+		return auditEntityMapper.toDomain(optional.orElse(null));
 	}
 
 	@Override
@@ -46,7 +52,16 @@ public class AuditEntityService implements AuditRepository{
 
 	@Override
 	public Audit save(Audit audit) {
-		return null;
+		AuditEntityValidate.validate(audit);
+		CinemaDate date = new CinemaDate();
+		audit.setId(0L);
+		audit.setIpLocal(!Cinema.isString(audit.getIpLocal()) ? Cinema.getIpLocal() : audit.getIpLocal());
+		audit.setIpPublic(!Cinema.isString(audit.getIpPublic()) ? Cinema.getIpPublic() : audit.getIpPublic());
+		audit.setDateRegister(date.currentToDateTime(null));
+		audit.setStatu(true);
+		AuditEntity auditEntity = auditEntityRepository.save(auditEntityMapper.toEntity(audit));
+		if (auditEntity == null)
+			throw new CinemaException("No se ha registrado la auditoria.");
+		return auditEntityMapper.toDomain(auditEntity);
 	}
-	
 }
